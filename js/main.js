c6fd3c654563625ce0e85706e5769ed5bf252757
Ashy-Sadao <ashy_is_beauty_level5@i.softbank.jp>
2022-01-27 20:33:40 +0900

    ブロック落下処理

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 // document.getElementById("hello_text").textContent="はじめてのJavascript";
+
+var count = 0;
+setInterval(function(){
+  // 何回目かを数えるために変数countを1ずつ増やす
+  count++;
+  // 何回目かを文字にまとめて表示する
+  document.getElementById("title").textContent = "さだおのホームページ(" + count + ")";
+  // 200この要素を持つ配列
+  var td_array = document.getElementsByTagName("td");
+  var cells =[];
+  var index = 0;
+  for(var row=0; row<20; row++){
+    // 配列のそれぞれの要素を配列にする
+    cells[row] = [];
+    for(var col=0; col<10; col++){
+      cells[row][col] = td_array[index];
+      index++;
+    }
+  }
+
+  // 一番下の行のクラスを空にする
+  for(var i=0; i<10; i++){
+    cells[19][i].className = "";
+  }
+
+  // 下から2番目の行から繰り返しクラスを下げて行く
+  for(var row=18; row >=0; row--){
+    for(var col=0; col < 10; col++){
+      if(cells[row][col].className != ""){
+        cells[row+1][col].className = cells[row][col].className;
+        cells[row][col].className = "";
+      }
+    }
+  }
+}, 1000);
